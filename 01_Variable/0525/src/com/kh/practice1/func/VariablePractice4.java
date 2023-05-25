b27a94ccfbbf1882302e412b95196951dbbc0b3b
ge4621 <ge4621@naver.com>
2023-05-25 17:45:08 +0900

    Update VariablePractice4.java

--- 01_Variable/0525/src/com/kh/practice1/func/VariablePractice4.java
+++ 01_Variable/0525/src/com/kh/practice1/func/VariablePractice4.java
@@ -1,5 +1,26 @@
 package com.kh.practice1.func;
 
+import java.util.Scanner;
+
 public class VariablePractice4 {
 
+		public void inputTest4() {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("문자열을 입력하세요 : ");
+			char str = sc.nextLine().charAt(0);
+			
+			System.out.print("첫 번째 문자 : " + );
+			char str = sc.nextLine().charAt(1);
+			
+			System.out.print("두 번째 문자 : ");
+			char str = sc.nextLine().charAt(2);
+			
+			System.out.print("세 번째 문자 : ");
+			char str = sc.nextLine().charAt(3);
+			
+			
+			
+		}
+	
 }

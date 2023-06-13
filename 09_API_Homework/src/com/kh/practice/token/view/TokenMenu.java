411b8ffc7b5d26ab4180c761bf830b57626369e9
ge4621 <ge4621@naver.com>
2023-06-14 00:28:20 +0900

    ..
    ..

--- 09_API_Homework/src/com/kh/practice/token/view/TokenMenu.java
+++ 09_API_Homework/src/com/kh/practice/token/view/TokenMenu.java
@@ -1,5 +1,22 @@
 package com.kh.practice.token.view;
 
+import java.util.Scanner;
+
+import com.kh.practice.token.controller.TokenController;
+
 public class TokenMenu {
+	
+	private Scanner sc = new Scanner(System.in);
+	private TokenController tc = new TokenController();
+	
+	public void mainMenu() {
+		
+	}
+	public void tokenMenu() {
+		
+	}
+	public void inputMenu() {
+		int count = 0;
+	}
 
 }

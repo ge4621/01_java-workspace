228f9ea50dee94be36c35755b3730304a023e431
ge4621 <ge4621@naver.com>
2023-06-15 01:54:26 +0900

    ..
    ..

---
+++ 09_API_Homework_윤지영/src/com/kh/practice/api/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.kh.practice.api.controller;
+
+public class BookController {
+
+	public BookController() {} //기본생성자
+	
+	
+	public void printAll() {
+		
+	}
+	
+	
+	
+}
---
+++ 09_API_Homework_윤지영/src/com/kh/practice/api/run/Run.java
@@ -0,0 +1,10 @@
+package com.kh.practice.api.run;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}

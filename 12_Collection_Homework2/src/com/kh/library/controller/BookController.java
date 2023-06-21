6679743967226419902fe255f9fe410bf922cd55
ge4621 <ge4621@naver.com>
2023-06-21 19:37:50 +0900

    ..
    ..

---
+++ 12_Collection_Homework2/src/com/kh/library/controller/BookController.java
@@ -0,0 +1,155 @@
+package com.kh.library.controller;
+
+import java.util.ArrayList;
+
+import com.kh.library.model.vo.Book;
+import com.kh.library.model.vo.Magazine;
+
+//BookManager 인터페이스 구현
+public class BookController implements BookManager  {
+	
+	ArrayList<Book> bList = new ArrayList<>();
+	
+	@Override
+	public void addBook(Book nBook) {
+		
+		bList.add(nBook);
+		
+	}
+
+	@Override
+	public ArrayList<Book> getAllBook() {
+		
+		
+		return bList;
+	}
+
+	@Override
+	public Book searchBookBybNo(String bNo) {
+		
+		Book b = null;
+		
+		for(int i = 0; i<bList.size(); i++) {
+			if(bList.get(i).getbNo().equals(bNo)) {
+				b = bList.get(i);
+			}
+		}
+		return b;
+	}
+
+	@Override
+	public ArrayList<Book> searchBookByTitle(String title) {
+		
+		ArrayList<Book> searchList = new ArrayList<>();
+		
+		for(int i = 0; i<bList.size();i++) {
+			if(bList.get(i).getTitle().contains(title)) {
+				searchList.add(bList.get(i));
+			}
+		}
+		
+		return searchList;
+	}
+
+	@Override
+	public ArrayList<Book> onlySearchBook() {
+		
+		ArrayList<Book> tempB = new ArrayList<>();
+		for(Book a :bList) {
+			if(a instanceof Magazine) {
+				continue;
+			}else {
+				tempB.add(a);
+			}
+		}
+		
+		return tempB;
+	}
+
+	@Override
+	public ArrayList<Book> onlySearchMagazine() {
+		
+		ArrayList<Book> tempM = new ArrayList<>();
+		for(Book a : bList) {
+			if(a instanceof Magazine) {
+				tempM.add(a);
+			}
+		}
+		
+		return tempM;
+	}
+
+	@Override
+	public ArrayList<Book> magazineOfThisYearInfo(int year) {
+		
+		ArrayList<Book> searchList = new ArrayList<>();
+		
+		for(int i = 0; i<bList.size();i++) {
+			
+			
+		}
+		
+		return null;
+	}
+
+	@Override
+	public ArrayList<Book> searchBookByPublisher(String publisher) {
+		
+		ArrayList<Book> searchList = new ArrayList<>();
+		
+		for(int i = 0; i<bList.size();i++) {
+			if(bList.get(i).getPublisher().contains(publisher)) {
+				searchList.add(bList.get(i));
+			}
+		}
+		
+		
+		return searchList;
+	}
+
+	@Override
+	public ArrayList<Book> searchBookByPrice(int price) {
+		
+		ArrayList<Book> searchList = new ArrayList<>();
+		
+		for(int i = 0; i<bList.size();i++) {
+			if(bList.get(i).getPrice()<price) {
+				searchList.add(bList.get(i));
+			}
+		}
+		
+		return searchList;
+	}
+
+	@Override
+	public int getTotalPrice() {
+		
+		
+		int sum = 0;
+		
+		for(int i = 0; i<bList.size();i++) {
+			sum += bList.get(i).getPrice();
+			sum++;
+			
+		}
+		
+		return sum;
+		
+	}
+
+	@Override
+	public int getAvgPrice() {
+		
+		//int result = 0;
+		int sum = 0;
+		
+		for(int i = 0; i<bList.size();i++) {
+			sum += bList.get(i).getPrice();
+			sum++;
+		}
+		
+		return sum/bList.size();
+	}
+	
+
+}
---
+++ 12_Collection_Homework2/src/com/kh/library/run/LibraryRun.java
@@ -0,0 +1,240 @@
+package com.kh.library.run;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.kh.library.controller.BookController;
+import com.kh.library.model.vo.Book;
+import com.kh.library.model.vo.Magazine;
+
+public class LibraryRun {
+
+	public static void main(String[] args) {// 소장책 DB
+		ArrayList<Book> bList = new ArrayList<>();
+
+	
+		// 생성 DB
+		
+		bList.add(new Book("100", "잃어버린 주원이를 찾아서", "저자 고경원", "상호 출판사", 8500, "『유정스』, 『르 몽드』 선정 20세기 최고의 책"));
+		bList.add(new Book("101", "현우의 자서전", "지완이가 옮김", "빛나는 현수출판사", 8500, "영화배우 민병진이 추천한 젊음에 바치는 영혼의 자서전"));
+		bList.add(new Magazine("102", "박경훈 과학동아", "편집부 박용진 선생", "혜수 사이언스", 8500, "관우의 유튜브에 소개된 과학만화 잡지", 2023,3));
+		bList.add(new Book("103", "관현이의 코딩스쿨", "코딩천재 윤재성", "정묵이네 동네", 9900, "제 13회 래언이가 주는 문학상 수상작"));
+		bList.add(new Magazine("104", "수진이와 지영이의 여행일기", "서울대 이승준 교수", "시연21", 3800, "영화감독 이원종pd가 집필한 영화 전문 잡지", 2023, 4));
+		bList.add(new Magazine("105", "호연이의 OOTD", "인플루언서 장희주", "유리네 책방", 12000, "핫가이 조대현의 샤라웃을 받은 잡지", 2022,11));
+		bList.add(new Book("106", "조준하의 미라클모닝", "샤이걸 성윤찡", "연우 출판사", 6300, "용석이도 2번이나 성공한 미라클모닝"));
+		bList.add(new Book("107", "민정이의 백만가지 요리레시피", "제주도에서 온 상원이", "우리반 출판사", 6300, "제주에서 밥해먹고 사는 일상"));
+		
+	
+		printBooks(bList);
+	}
+		
+
+	public static void printBooks(ArrayList<Book> bs) {
+		// 여기서 메뉴열고 구현 시작
+		BookController bc = new BookController();
+		
+		for(Book a : bs) {
+			System.out.println(a);
+			bc.addBook(a);
+		}
+		
+
+		boolean flag = true; // 메뉴 스위치
+
+		while (flag == true) { // 메뉴 스위치가 켜져있는 동안 무한반복
+			System.out.println("===============================");
+			System.out.println("KH 우리반전용 도서관 관리 페이지입니다.");
+			System.out.println("1. 조회하기");
+			System.out.println("2. 추가하기");
+			System.out.println("3. 책 찾기");
+			System.out.println("4. 전체책 가격 합계 및 평균 조회");
+			System.out.println("5. 프로그램 종료");
+			System.out.println("===============================");
+
+			Scanner sc = new Scanner(System.in);
+			System.out.print("메뉴번호를 입력해주세요 : ");
+			int menu = sc.nextInt(); // 메뉴 번호 입력
+			
+			switch (menu) {
+			case 1: // 1. 조회하기
+				System.out.println("===============================");
+				System.out.println("어떤 방법으로 책을 조회하겠습니까?");
+				System.out.println("1. 도서관 전체 소장책 조회하기");
+				System.out.println("2. 일반도서만 조회하기(잡지 제외)");
+				System.out.println("3. 잡지만 조회하기(일반도서 제외)");
+				System.out.println("4. 이전으로");
+				System.out.println("===============================");
+
+				sc.nextLine();
+
+				System.out.print("메뉴번호를 입력해주세요 : ");
+				int search = sc.nextInt();
+				
+				if (search == 1) {
+				ArrayList<Book> bList = bc.getAllBook();
+				
+				if(bList.isEmpty()) {
+					System.out.println("책이 존재하지 않습니다.");
+				}else {
+					for(int i = 0; i<bList.size();i++) {
+						System.out.println(bList.get(i)); 
+					}
+				}
+			}
+				else if (search == 2) {
+					
+					ArrayList<Book> tempB = bc.onlySearchBook();
+					for(int i = 0; i<tempB.size();i++) {
+						System.out.println(tempB.get(i));
+					}
+					
+				}
+				else if (search == 3) {
+					
+					ArrayList<Book> tempM = bc.onlySearchMagazine();
+					for(int i = 0; i<tempM.size();i++) {
+						System.out.println(tempM.get(i));
+					}
+					
+					
+					//onlySearchMagazine();
+				}
+				else if (search == 4) {
+					// 이전으로 가야함
+					
+				}
+				break;
+				
+			case 2: // 2. 추가하기
+				System.out.print("bNo을 입력하세요 : ");
+				String bNo = sc.nextLine();
+				sc.nextLine();
+				
+				System.out.print("책 제목을 입력하세요 : ");
+				sc.nextLine();
+				String title = sc.nextLine();
+				
+				System.out.print("작가를 입력하세요 : ");
+				String author = sc.nextLine();
+				
+				System.out.print("출판사를 입력하세요 : ");
+				String publisher = sc.nextLine();
+				
+				System.out.print("가격을 입력하세요 : ");
+				int price = sc.nextInt();
+				
+				sc.nextLine();
+				
+				System.out.print("간단한 설명을 입력하세요 : ");
+				String description = sc.nextLine();
+				
+				System.out.print("일반도서이면 true, 잡지이면 false를 입력하세요 : ");
+				Boolean bkOrMaga = sc.nextBoolean();
+				
+				if (bkOrMaga.equals(true)) {
+					
+				} else if (bkOrMaga.equals(false)) {
+					System.out.print("출간연도를 입력하세요 : ");
+					int year = sc.nextInt();
+					System.out.print("출간월을 입력하세요 : ");
+					int month = sc.nextInt();
+					
+				}
+				
+
+				break;
+				
+			case 3:// 3. 책 찾기
+				System.out.println("===============================");
+				System.out.println("어떤 방법으로 책을 찾겠습니까?");
+				System.out.println("1. bNo으로 책 찾기");
+				System.out.println("2. 책 제목으로 책 찾기");
+				System.out.println("3. 출간연도로 잡지 찾기");
+				System.out.println("4. 출판사로 책 찾기");
+				System.out.println("5. 특정 가격 밑으로 책 찾기");
+				System.out.println("6. 이전으로");
+				System.out.println("===============================");
+				System.out.print("메뉴번호를 입력해주세요 : ");
+				sc.nextLine();
+				search = sc.nextInt();
+				sc.nextLine();
+
+				if (search == 1) { // bNo으로 책 찾기
+					System.out.print("bNo을 입력하세요 : ");
+					bNo = sc.nextLine();
+					
+					System.out.println(bc.searchBookBybNo(bNo));
+			
+					
+				} else if (search == 2) { // 책 제목으로 책 찾기
+					System.out.print("책 제목을 입력하세요 : ");
+					//sc.nextLine();
+					
+					title = sc.nextLine();
+					
+					ArrayList<Book> searchList = bc.searchBookByTitle(title);
+					
+					for(int i =0 ; i<searchList.size();i++) {
+						System.out.println(searchList.get(i));
+						
+					}
+					
+					
+					//bc.searchBookByTitle()
+					
+				} else if (search == 3) { // 출간연도로 잡지 찾기
+					System.out.print("출간연도를 입력하세요 : (올해 --> 2022) : ");
+					int year = sc.nextInt();
+					
+					ArrayList<Book> searchList = bc.magazineOfThisYearInfo(year);
+					
+					
+					
+					//magazineOfThisYearInfo()
+					
+				} else if (search == 4) { // 4. 출판사로 책 찾기
+					System.out.print("출판사를 입력하세요 : ");
+					publisher = sc.nextLine();
+					
+					ArrayList<Book> searchList = bc.searchBookByPublisher(publisher);
+					
+					for(int i = 0; i<searchList.size();i++) {
+						System.out.println(searchList.get(i));
+					}
+					
+					//bc.searchBookByPublisher()
+					
+				} else if (search == 5) { // 5. 특정 가격 밑으로 책 찾기
+					System.out.print("가격을 입력하세요 : ");
+					price = sc.nextInt();
+					sc.nextLine();
+					
+					ArrayList<Book> searchList = bc.searchBookByPrice(price);
+					
+					for(int i = 0; i<searchList.size();i++) {
+						System.out.println(searchList.get(i));
+					}
+					
+					//bc.searchBookByPrice()
+					
+				} else if (search == 6)
+					flag = true; // 6. 이전으로
+				else
+					System.out.println("잘못 입력하셨습니다.");
+				
+				break;
+				
+			case 4:// 4. 전체책 가격 합계 및 평균 조회
+				System.out.println("전체책 가격 합계 : " + bc.getTotalPrice() + "원 입니다." );
+				System.out.println("전체책 가격 평균 : " + bc.getAvgPrice() + "원 입니다.");
+				break;
+				
+			case 5: 
+				System.out.println("프로그램을 종료합니다");
+				return;
+			default:
+				break;
+			}
+		}
+	}
+}
